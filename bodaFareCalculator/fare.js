9ce77626f40d7aff61ce0d23ef19f8340471393f
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-06-08 19:40:41 +0300

    create bodaFareCalculator ,chaiCalculator

---
+++ bodaFareCalculator/fare.js
@@ -0,0 +1,6 @@
+function calculateBodaFare(amount,distance){
+    let prompt ='Where are you going boss! How many kilometers?'
+   let distances = 'basFare +(distanceInKm* chargePerKm)'
+    console.log(prompt) ||console.log('um,Kisumu, I guess 76km')|| console.log('That will be'+'' + amount)
+}
+calculateBodaFare (' 400 ksh')
---
+++ chaiCalculator/chai.js
@@ -0,0 +1,14 @@
+function calculateChaiIngredients(){
+    const Greeting='Welcome to Chai Bora!'
+    let prompt= 'Hello! How many cups of tea would you like to make ?'
+    let numberOfCups = 7
+    let output ='To make you will need;'
+    const  water = '200 ml of water' 
+    const  milk  = '50 ml of milk' 
+    let teaLeaves  ='1 tablespoon of tealeaves' 
+    const sugar  = '2 teaspoons of sugar' 
+       console.log(numberOfCups) || console.log(output) || console.log(water) || console.log(milk) || console.log(teaLeaves )|| console.log(sugar)
+
+}
+calculateChaiIngredients(3) 
+
