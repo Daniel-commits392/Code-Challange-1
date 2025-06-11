68bdac202f350ed53efc5a646a543c983971f564
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-06-11 12:56:20 +0300

    Changes

--- mobileMoneyEstimator/fee.js
+++ mobileMoneyEstimator/fee.js
@@ -1,13 +1,20 @@
-function estimateTransactionFee(amountToSend ){
-    let prompt = 'How much are you sending ?'
-    let amount = 1.5/100 * 3000 + 3000
-    let message = 'Send money securely'
-    if (amount>=70) {
-        return  amount + 300}
-        else if(amount>=10){
-            return amount + 300
-        }
-    console.log (prompt) || console.log(amount) || console.log(message)
+function estimateTransactionFee(){
+    const amountStr =window.prompt('How much are you sending')
+    const amount = parseInt(amountStr)
+  
 
+let transactionCost = (amount * 1.5) /100
+if  (transactionCost<=10){
+    alert(`Sending KES: ${amount}
+          Calculated Transaction Fee: KES 10
+          TotalAmount To Be Debited : ${amount+10}`
+    )
+}
+if (transactionCost>=70){
+    alert(`Sending KES ; ${amount}
+           Calculated Transaction Fee :KES 70
+           Total Amount To Be Debited : ${amount +70}`)
+}
+console.log('Thanks for trusting us')
 }
 
