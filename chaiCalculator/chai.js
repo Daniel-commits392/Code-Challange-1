27e395cc80726a7846493dc3c6c42bb7454aaf82
Shadow Nexus <coderdaniel6853@gmail.com>
2026-02-06 16:00:52 +0300

    updated it to calculate the ingredients one needs to make tea regardless of the number of cups they want

--- chaiCalculator/chai.js
+++ chaiCalculator/chai.js
@@ -1,16 +1,30 @@
-function calculateChaiIngrindients(){
-    const noOfCups = window.prompt('How many cups of tea do you want?')
-    const numberOfCups = parseInt(noOfCups)
-    const  water =parseInt('200ml')
-    const  milk = parseInt('50ml')
-    const tealeaves = parseInt('1tablespoon')
-    const  sugar = parseInt('2 teaspoons')
-  let chaiCups = numberOfCups
-    alert (`To make ${numberOfCups} cups of Kenyan Chai, you will need
-            Water : ${numberOfCups * water } ml
-             Milk : ${numberOfCups * milk} ml
-         Tealeaves: ${numberOfCups * tealeaves} tablespoon
-             sugar :${numberOfCups * sugar} teaspoons`)
+let input =prompt('Karibu ! How many cups of Chai Bora would you like to make ?')
+let cups=Number(input)
+function calculateChaiIngredients(cups){
+  const water = 200 
+  const milk = 50
+  const teaLeaves= 1 
+  const sugar= 2 
+  let totalWater = cups * water 
+  let totalMilk  = cups * milk 
+  let totalTeaLeaves = cups * teaLeaves
+  let totalSugar = cups *sugar
+  console.log(`
+    To make ${cups} cups of Kenyan Chai, you will need :
+    Water : ${totalWater} ml
+    Milk : ${totalMilk} ml
+    Tea Leaves (Majani) : ${totalTeaLeaves} tablespoons
+    Sugar (Sukari) : ${totalSugar} teaspoons
 
+    😇 Enjoy your chai Bora 😇!
+    `)
 }
+if(isNaN(cups)|| cups<=0){
+  console.log('Please input a valid number')
+}else{
+  calculateChaiIngredients(cups)
+}
+
+
+
 
